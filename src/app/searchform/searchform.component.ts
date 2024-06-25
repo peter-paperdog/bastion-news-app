36cb492b2f89b942ba32f4f59a6c837f982e1c1c
peter <peter@paperdog.com>
2024-06-25 21:32:32 +0300

    Add search form component
    Implemented a new search form component with HTML and CSS coding. The form includes options for content type and categories with a submit button. Additionally, basic testing configuration for the new component has been set up.

---
+++ src/app/searchform/searchform.component.ts
@@ -0,0 +1,33 @@
+import {Component, EventEmitter, Output} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {NgForOf} from "@angular/common";
+
+@Component({
+  selector: 'app-searchform',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgForOf
+  ],
+  templateUrl: './searchform.component.html',
+  styleUrl: './searchform.component.css'
+})
+export class SearchformComponent {
+  @Output() search = new EventEmitter<string>();
+  searchForm: FormGroup;
+
+  contentTypes = ['All Content', 'Blog Posts', 'News'];
+  categories = ['All Categories', 'Category 1', 'Category 2', 'Category 3'];
+  constructor(private fb: FormBuilder) {
+    this.searchForm = this.fb.group({
+      contentType: ['All Content'],
+      category: ['All Categories'],
+      query: ['']
+    });
+  }
+
+  onSubmit(): void {
+    const query = this.searchForm.value.query;
+      this.search.emit(this.searchForm.value);
+  }
+}
