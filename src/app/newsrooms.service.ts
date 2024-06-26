afcb186502915d08269768518f5ef763992cd20b
peter <peter@paperdog.com>
2024-06-26 14:18:37 +0300

    Update NewsroomsService to use environment variables
    The code now uses the apiUrl and apiKey from the environment file, which increases flexibility and secures sensitive data. Moreover, the typeOfMedia default value has been changed to 'news' and a new method to search materials has been added, thus increasing the service's functionality.

--- src/app/newsrooms.service.ts
+++ src/app/newsrooms.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {environment} from '../environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsroomsService {
-  private apiUrl = 'https://www.mynewsdesk.com/services/pressroom'; // Base URL for the API
+  private apiUrl = environment.apiUrl;
+  private apiKey = environment.apiKey;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   // Method to list all materials
-  listMaterials(typeOfMedia: string = 'pressrelease', limit: number = 20, offset: number = 0, order: string = 'published'): Observable<any> {
+  listMaterials(typeOfMedia: string = 'news', limit: number = 20, offset: number = 0, order: string = 'published'): Observable<any> {
     let params = new HttpParams()
       .set('format', 'json')
       .set('type_of_media', typeOfMedia)
@@ -19,6 +22,41 @@ export class NewsroomsService {
       .set('offset', offset.toString())
       .set('order', order);
 
-    return this.http.get(`${this.apiUrl}/list/fxnW-qYxoiHF-AAVrX40zg`, { params });
+    return this.http.get(`${this.apiUrl}/list/${this.apiKey}`, {params});
+  }
+
+  // Method to search materials
+  searchMaterials(
+    query: string = '*',
+    // The string to search for. The search is performed as full text search
+    // on all text fields in the material, like header, summary and body.
+    // Wildcard search with "*" is also supported.
+
+    typeOfMedia: string = 'pressrelease',
+    limit: number = 20,
+    page: number = 1,
+    strict: boolean = true,
+    tags: string = '',
+    tagIds: string = '',
+    dateMode: string = 'between',
+    dateStart: string = '',
+    dateEnd: string = '',
+    pressroom: string = ''
+  ): Observable<any> {
+    let params = new HttpParams()
+      .set('format', 'json')
+      .set('query', query)
+      .set('type_of_media', typeOfMedia)
+      .set('limit', limit.toString())
+      .set('page', page.toString())
+      .set('strict', strict.toString())
+      .set('pressroom', pressroom)
+      .set('tags', tags)
+      .set('tag_ids', tagIds)
+      .set('date_mode', dateMode)
+      .set('date_start', dateStart)
+      .set('date_end', dateEnd);
+
+    return this.http.get(`${this.apiUrl}/search/${this.apiKey}`, {params});
   }
 }
