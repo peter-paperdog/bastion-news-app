ca6c39065340a7981561a3878090f2f7a3efe01b
peter <peter@paperdog.com>
2024-06-26 14:17:57 +0300

    Add configuration files for production and development environments
    New configuration files, `environment.prod.ts` and `environment.ts` have been added. These files set the configuration for production and development environments respectively, including the API key and URL.

---
+++ src/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://www.mynewsdesk.com/services/pressroom',
+  apiKey: 'fxnW-qYxoiHF-AAVrX40zg'
+};
---
+++ src/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://www.mynewsdesk.com/services/pressroom',
+  apiKey: 'fxnW-qYxoiHF-AAVrX40zg'
+};
